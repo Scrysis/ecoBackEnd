491c0f68563645a735bf4f4829660ac3477f80f3
Scrysis <Scrysis@users.noreply.github.com>
2024-01-17 02:52:00 -0700

    Modified a few files; still having Eager Loading error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
-const sequelize = require("sequelize");
+const sequelize = require("./config/connection");
+//const sequelize = require("sequelize");
 // import sequelize connection
 
 const app = express();
