cf8e877b73d9649bb71250a1731618c9c4b06d5d
Scrysis <Scrysis@users.noreply.github.com>
2024-03-31 15:09:03 -0700

    updating

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 try{
   const categoryData = await Category.findAll({
-    include: [{ model: Product, as: `product`}]
+    include: { all: true, nested: true }
   });
   res.status(200).json(categoryData);
 } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try{
     const categoryData = await Category.findByPk(req.params.id,{
-      include: [{ model: Product, as: `product`}]
+      include: { all: true, nested: true }
     });
     if (!categoryData){
       res.status(404).json({ message: 'No category with that id found.'});
